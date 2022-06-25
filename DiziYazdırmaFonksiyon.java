77b474bd6ff76e0f2bddb0dec26a4a35d674a479
lxyerx <95114297+lxyerx@users.noreply.github.com>
2022-06-26 02:00:58 +0300

    Create DiziYazdırmaFonksiyon.java

---
+++ DiziYazdırmaFonksiyon.java
@@ -0,0 +1,11 @@
+	public static void printarray(int[] p) { // 
+		for (int k = 0; k < p.length; k++) { // dizi yazdirma fonksiyonu
+			System.out.println(p[k]);
+		}
+	}
+
+	public static void array(int[] j) {  // Foreach fonksiyon
+		for (int v : j) {
+			System.out.println(v);
+		}
+	}
