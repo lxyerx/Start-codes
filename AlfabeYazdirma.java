480c7596fe471937d9391bac475128779fb80540
lxyerx <95114297+lxyerx@users.noreply.github.com>
2022-06-26 02:35:21 +0300

    Update AlfabeYazdirma.java

--- AlfabeYazdirma.java
+++ AlfabeYazdirma.java
@@ -1,5 +1,14 @@
 public static void main(String[] args) {
+	
+		System.out.println("A'dan Z'ye Yazdirma");
 		for (char i = 'A'; i <= 'Z'; i++) {
 			System.out.print(" " + i);
 		}
+		System.out.println("Z'den A'ya Yazd�rma");
+		for (char k = 'Z'; k >= 'A'; k--) {
+			System.out.print(" " + k);
+
+		}
+	
+	
 	}
