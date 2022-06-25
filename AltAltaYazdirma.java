582696f898e4bb8f69dfc736855f0b5489e88560
lxyerx <95114297+lxyerx@users.noreply.github.com>
2022-06-26 02:39:36 +0300

    Create AltAltaYazdirma.java

---
+++ AltAltaYazdirma.java
@@ -0,0 +1,10 @@
+	public static void main(String[] args) {
+
+		System.out.println("Alt alta yazdirma kodu);
+		String isim = "Mustafa";
+		char olcum = isim.charAt(isim.length() - 1);
+		for (int q = 0; q <= isim.length() - 1; q++) {
+			System.out.println(isim.charAt(q));
+		}
+		
+	}
