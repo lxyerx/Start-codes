a32107b3b6568f2a5ff0a59ce4fe7e6f695db043
lxyerx <95114297+lxyerx@users.noreply.github.com>
2022-06-26 02:29:19 +0300

    Create TekCift.java

---
+++ TekCift.java
@@ -0,0 +1,19 @@
+public static void tekCift(int n){
+  
+  	if (n % 2 == 0) {
+			System.out.println("Cifttir");
+		} else {
+			System.out.println("Tektir");
+		}
+  
+}
+public static void main(String[] args) {
+
+		int sayi = 2;
+		if (sayi % 2 == 0) {
+			System.out.println("Cifttir");
+		} else {
+			System.out.println("Tektir");
+		}
+
+	}
