c3d90cd59dc3c386fca6e55cf80656712a35c773
lxyerx <95114297+lxyerx@users.noreply.github.com>
2022-06-26 02:56:55 +0300

    Create EnBuyukBulma.java
    eşitlik varsa Else blogu çalışır

---
+++ EnBuyukBulma.java
@@ -0,0 +1,18 @@
+	public static void main(String[] args) {
+
+		System.out.println("En buyuk sayiyi bulma");
+
+		int a = 2;
+		int b = 5;
+		int c = 15;
+
+		if (a > b && a > c) {
+			System.out.println("A en buyugu");
+
+		} else if (b > a && b > c) {
+			System.out.println("B en buyugu");
+		} else {
+			System.out.println("C en buyugu");
+		}
+
+	}
