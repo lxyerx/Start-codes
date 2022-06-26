0f738eb3a34250689615c1c65e6672efe992f9e0
lxyerx <95114297+lxyerx@users.noreply.github.com>
2022-06-26 03:12:48 +0300

    Create ForCarpimTablosu.java
    ilk öğrendiğimde keyif vermişti çarpım tablosu

---
+++ ForCarpimTablosu.java
@@ -0,0 +1,10 @@
+	public static void main(String[] args) {
+
+		for (int k = 1; k <= 10; k++) {
+			for (int q = 1; q <= 10; q++) { // 1 den 10 a carpim tablosu
+				System.out.print("\t" + k * q);
+			}
+			System.out.println("");
+
+		}
+	}
