befb1e6fbe71b725caf84813ed212d13abd26f11
lxyerx <95114297+lxyerx@users.noreply.github.com>
2022-06-26 03:34:06 +0300

    Create BasamakSayisiBulma.java
    basamak sayısı sayının kaç rakamdan oluştuğunu bulma

---
+++ BasamakSayisiBulma.java
@@ -0,0 +1,15 @@
+   	public static void main(String[] args) {
+      
+          int sayi = 12345;
+
+		      int sayac = 0;
+		      while (sayi != 0) {
+
+			    sayi /= 10;
+			    sayac++;
+		      }
+		      System.out.println(sayac);
+
+  
+      
+   }
