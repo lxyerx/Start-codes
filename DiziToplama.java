5348728db5ebec1977195542895f24d25493bd7a
lxyerx <95114297+lxyerx@users.noreply.github.com>
2022-07-01 03:46:19 +0300

    Update DiziToplama.java

--- DiziToplama.java
+++ DiziToplama.java
@@ -9,5 +9,16 @@
 			toplam += dizi1[i] + dizi2[i];
 		}
 		System.out.println(toplam);
+	}
+		// alttaki ise bir dizinin elemanlarını yan yana yazıp toplar
 
+	public static void main(String[] args) {
+
+		int[] dizi1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int toplam = 0;
+		for (int i = 0; i < dizi1.length; i++) {
+			System.out.print(dizi1[i]+"+");
+			toplam += dizi1[i];
+		}
+		System.out.println(" = "+toplam);
 	}
