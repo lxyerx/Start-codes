9273531cc2bcb4560773c254eb4fc568ddb6f0bd
lxyerx <95114297+lxyerx@users.noreply.github.com>
2022-07-01 03:58:06 +0300

    Update DiziCiftSayisiBulma.java

--- DiziCiftSayisiBulma.java
+++ DiziCiftSayisiBulma.java
@@ -14,3 +14,28 @@
 		}
 		System.out.print("Cift sayısı " + sayac);
 	}
+	// alttaki hazır olan dizinin elemanlarını fonksiyon ile sorgular ve çiftleri yazdırır klavyeden giris yoktur
+
+	public static boolean Tekmi(int n) {
+
+		if (n % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		Scanner klavye = new Scanner(System.in);
+		int sayac = 0;
+		int[] dizi1 = { 2, 4, 6, 8, 10, 12, 11, 14, 18, 1, 3, 5, 7 };
+
+		for (int i = 0; i < dizi1.length; i++) {
+			if (Tekmi(dizi1[i])) {
+				sayac++;
+			}
+		}
+		System.out.println("Cift sayisi " + sayac);
+	}
